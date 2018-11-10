1a49dc507f084ff7d155570a35521365e94c814c
UmaSenthil <us186007@teradata.com>
2018-11-10 15:41:21 -0800

    Skype support

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,20 @@
 class PatientsController < ApplicationController
+	def index
+		@patients = Patient.paginate(:page => params[:page], :per_page => 3)
+	end
+
+	def new
+	  @patient = Patient.new
+	end
+
+    def create
+      binding.pry
+  	  current_user.create_patient(patient_params)
+  	  redirect_to static_pages_home_path
+    end
+
+    private
+    def patient_params
+      params.require(:patient).permit(:name, :skypeid)
+    end
 end
